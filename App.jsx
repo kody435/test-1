af7c7aca4810bac48348a61e481a902d9e88acf4
Cody <50862704+kody435@users.noreply.github.com>
2022-05-25 08:48:34 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Screen1 from './Screen1'
+
+const App = () => {
+
+  return (
+      <div>
+      <Screen1 />
+    </div>
+  )
+}
+
+export default App
---
+++ Screen1.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Screen1 = () => {
+
+    return (
+        <div className="App">
+            <div className="header">
+                Welcome  to
+                <br></br>
+                Portfolio
+                <br></br><br></br>
+          <div className='div' >
+            <button>EXPLORE MORE</button>
+            <div className='arrow'>↗</div>
+          </div>
+            </div>
+        </div>
+    )
+}
+
+export default Screen1
